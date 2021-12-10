2846f5090a2220735ec2159d90811ffc00e35c8d
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:11:16 +0530

    python_basics_1\class1_conditionalstatements.py

---
+++ python_basics_1/class5_conditionalstatements.py
@@ -0,0 +1,37 @@
+#conditional statements
+'''a=0
+b=1
+print(a==b)'''
+'''a!=b
+a<=b
+a>=b'''
+a=1
+b=2
+c=3
+#syntax
+'''if False:
+    print("this is if")
+else:
+    print("this is else")'''
+'''if a>b:
+    print("a is bigger than b")
+elif a<c:
+    print("a is lesser than c")
+else:
+    print("a is not bigger than b")'''
+#short hand if
+'''a=1
+b=2
+print(b)if a<b else print(a)'''
+
+#logical operators
+#and or
+'''a=1
+b=2
+c=3
+if a>b or c>a:
+    print("this is or operator")
+else:
+    print("this is not or operator")'''
+#atm managment(input, float,if else,variables,operators)
+#login authentication
