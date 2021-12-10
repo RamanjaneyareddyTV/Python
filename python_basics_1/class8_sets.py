750f09575a4a1d612e750241d93b3595c3c17181
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:18:39 +0530

    python_basics_1\class8_sets.py

---
+++ python_basics_1/class8_sets.py
@@ -0,0 +1,30 @@
+import numpy as np
+#sets
+'''
+d={1,2,3,4,2,"kiran","kiran"}
+d.remove("venkat")
+'''
+
+#remove
+#pop
+#union(|)
+'''d1={1,2,3,4,5,6}
+d2={2,3,4,5,6,7}
+print(d1|d2)'''
+#intersection(&)
+'''d1={1,2,3,4,5,6}
+d2={2,3,4,5,6,7}
+print(d1&d2)'''
+#difference
+'''d1={1,2,3}
+d2={4,5,6}
+print(d1.isdisjoint(d2))'''
+# frozen-set
+
+
+'''
+s="kiran"
+k=1
+d=s+k
+print(d)
+'''
