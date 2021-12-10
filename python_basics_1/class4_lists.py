3a2e3e2cb5f234612a9c3f743f364b4ac796ff49
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:10:52 +0530

    python_basics_1\class4_lists.py

---
+++ python_basics_1/class4_lists.py
@@ -0,0 +1,37 @@
+#lists          
+lists=[]#empty list
+venkat=['kiran',4,2.0,True,'kiran']
+#lists functions
+'''venkat.insert(1,'venkat')
+print(venkat)'''
+#append
+'''venkat.append("python")
+print(venkat)'''
+#extend
+'''a=[1,2,3]
+b=[2,3,4]
+a.extend(b)
+print(a)'''
+#remove
+'''venkat.remove("kiran")
+print(venkat)'''
+#pop
+'''venkat.pop(-1)
+print(venkat)'''
+#del
+'''a=[1,2,3,4]
+del a
+print(a)'''
+#clear
+'''a=[1,2,3,4]
+a.clear()
+print(a)'''
+
+
+
+
+
+
+
+
+
