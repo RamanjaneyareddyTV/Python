1a8fa8e0cc7aa3431cdc5404ea2b18eca525f7d1
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:10:35 +0530

    python_basics_1\class3_strings.py

---
+++ python_basics_1/class3_strings.py
@@ -0,0 +1,73 @@
+#strings
+'''venkat1='kiran'
+venkat2="kiran"
+venkat3=''
+print(type(venkat1))
+print(type(venkat2))
+print(type(venkat3))'''
+#define 
+'''a='venkat'
+print(a)'''
+'''a=2
+#converting the int to string but not to string to int
+b='venkat'
+c=str(a)+b
+print(c)'''
+'''
+#indexing the values accessing the string characters
+a='venkat'
+print(a[3])
+#len function
+b='this is venkat'
+print(len(b))'''
+#check string in and not
+'''text="venkat is good person"
+print("bad" not in text)'''
+#slicing
+'''d='this is string'
+print(d[::-2])'''
+#upper
+'''d="my name is kiran"
+print(d)
+print(d.upper())'''
+#lower
+'''d="My name is kiran"
+print(d)
+print(d.lower())'''
+#strip
+'''d="     my name is kiran    "
+print(d)
+print(d.strip())'''
+#replace
+'''d="my name is kiran"
+print(d)
+old=str(input("enter the old value"))
+new=str(input("enter the new value"))
+print(d.replace(old,new))'''
+#split
+'''d="my,name is kiran"
+print(d)
+print(d.split(","))'''
+#format
+'''string="this is {} my age is {}"
+print(string.format("kiran",22))
+d='kiran'
+e='\n'
+c='venkat'
+'''
+print("this is '\n' venkat")
+#join
+'''s=("kiran","venkat")
+x="".join(s)
+print(x)'''
+'''s='kiran'
+c='venkat'
+d=s+c
+print(d)'''
+#count
+'''s="this is kiran is"
+print(s.count('is',3))'''
+#task1 paragraph is are and count
+#task2 all string functions u have to practise
+
+
