e76c7b036558e274b3e23a54295734d20596f3f9
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:26:20 +0530

    python_basics_1\class9_random.py

---
+++ python_basics_1/class9_random.py
@@ -0,0 +1,10 @@
+import random
+#random.randint
+#print(random.randint(1,10))
+#l=['kiran','venkat','python']
+#random.shuffle(l)
+#print(l)
+#l=['kiran','venkat','python']
+#print(random.sample(l,k=2))
+#l=['kiran','venkat','python']
+#print(random.choice(l))
