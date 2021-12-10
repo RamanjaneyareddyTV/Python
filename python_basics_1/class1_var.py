e990397b5262d18ec6fc25e1ffcf3e0d04ac8d61
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:09:43 +0530

    python_basics_1\class1_var.py

---
+++ python_basics_1/class1_var.py
@@ -0,0 +1,57 @@
+#comments
+'''
+1.single line
+2.multi line
+'''
+#this is single line comment
+
+'''
+this is multi line comment
+'''
+#variables
+'''venkat=10
+venkat1=10.1
+venkat_float=10.2 #float variable
+venkat_string='a' #string variable
+venkat_bool=True  #boolean variable
+venkat_complex=10+2j
+#print(venkat_string)
+
+a=10
+b=20
+c=a+b
+print("addtion:",c+a)
+
+
+add=12
+sub=20
+mul=add*sub
+temp=mul
+print(mul)
+print(temp)'''
+
+
+'''x=y=z='venkat'
+print(x+y+z)'''
+
+'''x,y,z=10,'venkat',True
+print(x,y,z)'''
+
+#1.task 10 students roll_num group
+#2.simple intrest
+#3.compound intrest
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
