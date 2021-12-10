694e806142c3b9e6e9f0d27cd7dee3a2548c695d
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:10:19 +0530

    python_basics_1\class2_datatypes.py

---
+++ python_basics_1/class2_datatypes.py
@@ -0,0 +1,41 @@
+#datatypes
+'''
+1.premitive/fundamental
+2.dervied datatypes '''
+'''1.integer
+2.float
+3.string
+4.boolean
+5.complex'''
+'''1.list
+   2.dictionary
+   3.set
+   4.tuple'''
+
+'''a=10  #integer
+b=2.3 #float
+c='venkat' #string
+d=False #boolean
+f=1+2j #complex
+#type function
+print(type(f))'''
+'''
+
+#INPUT
+name=str(input("Enter your name:"))
+print(name)
+
+'''
+#empname
+#empage
+#empsalary
+#married
+'''
+-------------------------------------------------------
+                      -Emp details-
+emp-name  :
+emp-age   :
+emp-salary:
+Married   :
+---------------------------------------------------------
+'''
