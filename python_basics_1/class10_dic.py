c3a2c9586d1cb58b35f3bf6334dbba821e5f7fba
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:26:38 +0530

    python_basics_1\class10_dic.py

---
+++ python_basics_1/class10_dic.py
@@ -0,0 +1,55 @@
+d={'name':'venkat','number':[7777,9999]}
+#print(type(d))
+#dic()
+#update
+#d.update({'age':25})
+#print(d)
+#add
+'''d['name']='kiran'
+print(d)'''
+'''d.pop()
+print(d)'''
+#del
+'''del d['name']
+print(d)'''
+'''for x,y in d.items():
+    print(x,y)'''
+'''l=[1,2,3,4]
+d=l.copy()
+
+l.extend([1])
+print(l)
+print(d)'''
+#super market bill generation
+'''
+1.lists of items and its rates
+2.customer name
+3.item and quantity
+4.q*item rate
+5.items and quantity gst
+-------------------------------------------------------
+                  venkat super market
+name:
+sno items              quantity                    rate   
+1   sugar                 2                         20
+2   sugar                 2                         20
+3   sugar                 2                         20
+                                      total bill   :60
+                                      gst          :65
+                Thanks for shopping
+                    visit again
+--------------------------------------------------------
+'''
+#guess-game
+''' 1 to 5
+user'''
+'''import class11_function
+print(class11_function.add1(1,2))'''
+
+
+
+
+
+
+
+
