8c245aa2e9659ef1227b693b9ee61f477203acdc
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:18:14 +0530

    python_basics_1\class7_tuples.py

---
+++ python_basics_1/class7_tuples.py
@@ -0,0 +1,21 @@
+#Tuples
+#unmutable
+#multiple-datatypes
+'''d=(1,2,'kiran')
+print(type(d))'''
+'''p=[1,2,4]
+d=tuple(p)'''
+#len
+'''d=(1,2,'kiran')
+print(len(d))'''
+#imp interview
+'''
+l=[1,2,3,[1,2,3]]
+print(l[3][2])
+'''
+#index
+'''
+l=(1,2,3,3,4,5)
+print(l[1])
+'''
+
