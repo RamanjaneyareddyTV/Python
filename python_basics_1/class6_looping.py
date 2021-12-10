d02e5e420a3443386f3f1ee2df5d603b8d0e0f67
Ramanjaneyareddy <Ramanjaneyareddy3043@outlook.com>
2021-12-10 12:17:32 +0530

    python_basics_1\class6_looping.py

---
+++ python_basics_1/class6_looping.py
@@ -0,0 +1,36 @@
+#looping statements
+#for loop
+#while loop
+'''
+for x in 'venkat':
+    print(x)
+    '''
+'''l=['python','c','c++','java','ruby','go']
+for x in l:
+    if x=='python':
+        print("this is python")
+    elif x=='c++':
+        continue'''
+#range
+'''
+for x in range(2,10,3):
+    print(x)'''
+#pass
+'''for x in []:
+    pass'''
+'''
+while True:
+    print("this is while")
+'''    
+'''
+i=0
+while i<=9:
+    for x in
+        for x in
+    print("while")
+    i+=1
+else:
+    print("looping is stoped")
+    '''
+#task l=5 strings 5 int 5 5 float 
+
